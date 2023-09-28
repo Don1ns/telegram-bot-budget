308450d193200b4a544a67bc1fc50b77a19a1311
Riyaz Karimullin <riaz.karimullin@yandex.ru>
2023-09-28 18:47:48 +0300

    Initial commit

---
+++ src/main/java/me/don1ns/telegrambudgetbot/TelegrambudgetbotApplication.java
@@ -0,0 +1,13 @@
+package me.don1ns.telegrambudgetbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambudgetbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambudgetbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/don1ns/telegrambudgetbot/TelegrambudgetbotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.don1ns.telegrambudgetbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambudgetbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
